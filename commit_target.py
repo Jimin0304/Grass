15e197ae7f37b28c849b3e87482243861340249c
Jimin0304 <pjws0304@naver.com>
2025-05-25 10:10:26 +0900

    자동 커밋: 2025-05-25 10:10:26

---
+++ commit_target.py
@@ -0,0 +1,16 @@
+* [funcy](https://github.com/Suor/funcy) - A fancy and practical functional tools.
+
+## Processes
+
+- [Resources](#resources)
+
+
+
+    * [csvkit](https://github.com/wireservice/csvkit) - Utilities for converting to and working with CSV.
+
+    * [PyCharm](https://www.jetbrains.com/pycharm/) - Commercial Python IDE by JetBrains. Has free community edition available.
+
+    * [sanic](https://github.com/sanic-org/sanic) - A Python 3.6+ web server and web framework that's written to go fast.
+
+## Microsoft Windows
+
